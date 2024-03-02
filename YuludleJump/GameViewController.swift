97eb94b7d9502e6d4d6ce7dee813a07e9253703c
Ilya Egorov <>
2024-03-02 21:39:15 +0100

    Добавил подпись Pause

--- YuludleJump/GameViewController.swift
+++ YuludleJump/GameViewController.swift
@@ -22,6 +22,7 @@ final class GameViewController: UIViewController {
     @IBOutlet private weak var scoreboardView: UIView!
     @IBOutlet private weak var pauseButton: UIButton!
     @IBOutlet private weak var blurView: UIView!
+    private let pauseLabel = UILabel()
     @IBOutlet private weak var scoreLabel: UILabel!
     @IBOutlet private weak var livesLabel: UILabel!
     
@@ -44,14 +45,29 @@ final class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
         scoreboardView.isHidden = true
         blurView.isHidden = true
-        
+        pauseLabel.isHidden = true
+     
+        setupPauseLabel()
         ResizedImages.configure(width: view.frame.width) { [weak self] in
             let menuScene = (self?.view as? SKView)?.scene as? MenuScene
             menuScene?.setIsReady()
         }
+    }
+    
+    private func setupPauseLabel() {
+        pauseLabel.text = "Pause".uppercased()
+        pauseLabel.font = .systemFont(ofSize: 24, weight: .bold)
+        pauseLabel.textColor = .white
+        pauseLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(pauseLabel)
+        NSLayoutConstraint.activate([
+            pauseLabel.centerXAnchor.constraint(equalTo: blurView.centerXAnchor),
+            pauseLabel.centerYAnchor.constraint(equalTo: blurView.centerYAnchor)
+        ])
     }
     
     override func viewSafeAreaInsetsDidChange() {
@@ -60,11 +76,8 @@ final class GameViewController: UIViewController {
         if let view = self.view as? SKView {
             
             let menuScene = MenuScene(size: view.bounds.size, gameDelegate: self)
-            
             menuScene.scaleMode = .aspectFill
-            
             view.presentScene(menuScene)
-            
             view.ignoresSiblingOrder = true
         }
     }
@@ -87,16 +100,19 @@ final class GameViewController: UIViewController {
         pauseButton.setImage(pauseImage, for: .normal)
         
         if isPaused {
-            self.blurView.isHidden = false
+            blurView.isHidden = false
+            pauseLabel.isHidden = false
             (view as? SKView)?.isPaused = isPaused
         }
         UIView.animate(
             withDuration: 0.3,
             animations: { [unowned self] in
                 self.blurView.alpha = isPaused ? 0.6 : 0.0
+                self.pauseLabel.alpha = isPaused ? 1.0 : 0.0
             }, completion: { [weak self] _ in
                 if !isPaused {
                     self?.blurView.isHidden = true
+                    self?.pauseLabel.isHidden = true
                     (self?.view as? SKView)?.isPaused = isPaused
                 }
                 self?.pauseButton.isUserInteractionEnabled = true
